ec5130fbbb27945fbf4292c2d7bc0570e5b1cbcd
Denis <varenikovda@gmail.com>
2022-02-10 11:47:37 +0300

    Update file project

---
+++ colorSettingHW6/ViewControllers/ColorSettingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ColorSettingViewController.swift
+//  colorSettingHW6
+//
+//  Created by Денис Вареников on 10.02.2022.
+//
+
+import UIKit
+
+class ColorSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
