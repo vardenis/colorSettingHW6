3ea17dfb851ec9d46957d5fa05cf4d30bd63f8f8
Denis <varenikovda@gmail.com>
2022-02-10 11:41:52 +0300

    Initial Commit

---
+++ colorSettingHW6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  colorSettingHW6
+//
+//  Created by Денис Вареников on 10.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
