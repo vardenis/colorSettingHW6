b59a8ea739975e8ce40f451b6715c7b2b8d89e58
Denis <varenikovda@gmail.com>
2022-02-10 16:32:22 +0300

    Update file

--- colorSettingHW6/ViewControllers/ColorSettingViewController.swift
+++ colorSettingHW6/ViewControllers/ColorSettingViewController.swift
@@ -9,21 +9,63 @@ import UIKit
 
 class ColorSettingViewController: UIViewController {
 
+
+    @IBOutlet var labelRed: UILabel!
+    @IBOutlet var labelGreen: UILabel!
+    @IBOutlet var labelBlue: UILabel!
+    
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    
+    @IBOutlet var redTF: UITextField!
+    @IBOutlet var greenTF: UITextField!
+    @IBOutlet var blueTF: UITextField!
+    
+    
+    @IBOutlet var colorWindow: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        labelRed.text = String(format: "%.2f", sliderRed.value)
+        labelGreen.text = String(format: "%.2f", sliderGreen.value)
+        labelBlue.text = String(format: "%.2f", sliderBlue.value)
+        
+        redTF.text = String(format: "%.2f", sliderRed.value)
+        greenTF.text = String(format: "%.2f", sliderGreen.value)
+        blueTF.text = String(format: "%.2f", sliderBlue.value)
+        
+        colorWindow.layer.cornerRadius = 10
+        setColor()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func sliderAction(_ sender: UISlider) {
+        
+        switch sender {
+        case sliderRed:
+            labelRed.text = String(format: "%.2f", sliderRed.value)
+            redTF.text = String(format: "%.2f", sliderRed.value)
+        case sliderGreen:
+            labelGreen.text = String(format: "%.2f", sliderGreen.value)
+            greenTF.text = String(format: "%.2f", sliderGreen.value)
+        default:
+            labelBlue.text = String(format: "%.2f", sliderBlue.value)
+            blueTF.text = String(format: "%.2f", sliderBlue.value)
+        }
+        
+        setColor()
     }
-    */
+      
 
+}
+
+extension ColorSettingViewController {
+    private func setColor() {
+        colorWindow.backgroundColor = UIColor(
+            red: CGFloat(sliderRed.value),
+            green: CGFloat(sliderGreen.value),
+            blue: CGFloat(sliderBlue.value),
+            alpha: 1)
+    }
 }
--- colorSettingHW6/ViewControllers/StartViewController.swift
+++ colorSettingHW6/ViewControllers/StartViewController.swift
@@ -7,23 +7,30 @@
 
 import UIKit
 
+/*protocol ColorSettingViewControllerDelegate {
+    func setBackgroundColor(red: CGFloat, green: CGFloat, blue: CGFloat)
+    
+}*/
+
 class StartViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        
     }
-    */
 
 }
+
+/*extension StartViewController: ColorSettingViewControllerDelegate {
+    func setBackgroundColor(red: CGFloat, green: CGFloat) {
+        <#code#>
+    }
+    
+    
+}*/
